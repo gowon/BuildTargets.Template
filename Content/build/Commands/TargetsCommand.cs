f33d6fa14301b1d6f694369ac3669cb19b0940e1
Gowon Patterson <contact@gowon.dev>
2023-08-16 00:20:23 -0400

    Refactored base of application to use System.Commandline.
    Moved Bullsete targets to subcommand.
    Added generic host pattern implementation.

---
+++ Content/build/Commands/TargetsCommand.cs
@@ -0,0 +1,118 @@
+﻿namespace build.Commands;
+
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using Bullseye;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+public class TargetsCommand : Command
+{
+    private const string ArtifactsDirectory = ".artifacts";
+    private const string TestResultsDirectory = ".test-results";
+
+    private static readonly Option<string> ConfigurationOption =
+        new(new[] { "--configuration", "-C" }, () => "Release", "The configuration to run the target");
+
+    public TargetsCommand() : base("targets", "Execute build targets.")
+    {
+        AddOption(ConfigurationOption);
+
+        ImportBullseyeConfigurations();
+
+        this.SetHandler(async context =>
+        {
+            Target(Targets.RestoreTools, async () => { await RunAsync("dotnet", "tool restore"); });
+
+            Target(Targets.CleanArtifactsOutput, () =>
+            {
+                if (Directory.Exists(ArtifactsDirectory))
+                {
+                    Directory.Delete(ArtifactsDirectory, true);
+                }
+            });
+
+            Target(Targets.CleanTestsOutput, () =>
+            {
+                if (Directory.Exists(TestResultsDirectory))
+                {
+                    Directory.Delete(TestResultsDirectory, true);
+                }
+            });
+
+            Target(Targets.CleanBuildOutput, async () =>
+            {
+                var configuration = context.ParseResult.GetValueForOption(ConfigurationOption);
+                await RunAsync("dotnet", $"clean -c {configuration} -v m --nologo");
+            });
+
+            Target(Targets.CleanAll,
+                DependsOn(Targets.CleanArtifactsOutput, Targets.CleanTestsOutput, Targets.CleanBuildOutput));
+
+            Target(Targets.Build, DependsOn(Targets.CleanBuildOutput), async () =>
+            {
+                var configuration = context.ParseResult.GetValueForOption(ConfigurationOption);
+                await RunAsync("dotnet", $"build -c {configuration} --nologo");
+            });
+
+            Target(Targets.Pack, DependsOn(Targets.CleanArtifactsOutput, Targets.Build), async () =>
+            {
+                var configuration = context.ParseResult.GetValueForOption(ConfigurationOption);
+                await RunAsync("dotnet",
+                    $"pack -c {configuration} -o {Directory.CreateDirectory(ArtifactsDirectory).FullName} --no-build --nologo");
+            });
+
+            Target(Targets.PublishArtifacts, DependsOn(Targets.Pack), () => Console.WriteLine("publish artifacts"));
+
+            Target("default", DependsOn(Targets.RunTests, Targets.PublishArtifacts));
+
+            Target(Targets.RunTests, DependsOn(Targets.CleanTestsOutput, Targets.Build), async () =>
+            {
+                var configuration = context.ParseResult.GetValueForOption(ConfigurationOption);
+                await RunAsync("dotnet",
+                    $"test -c {configuration} --no-build --nologo --collect:\"XPlat Code Coverage\" --results-directory {TestResultsDirectory}");
+            });
+
+            Target(Targets.RunTestsCoverage, DependsOn(Targets.RestoreTools, Targets.RunTests), () =>
+                Run("dotnet",
+                    $"reportgenerator -reports:{TestResultsDirectory}/**/*cobertura.xml -targetdir:{TestResultsDirectory}/coveragereport -reporttypes:HtmlSummary"));
+
+            await RunBullseyeTargetsAsync(context);
+        });
+    }
+
+    private void ImportBullseyeConfigurations()
+    {
+        Add(new Argument<string[]>("targets")
+        {
+            Description =
+                "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed. Target names may be abbreviated. For example, \"b\" for \"build\"."
+        });
+
+        foreach (var (aliases, description) in Bullseye.Options.Definitions)
+            Add(new Option<bool>(aliases.ToArray(), description));
+    }
+
+    private async Task RunBullseyeTargetsAsync(InvocationContext context)
+    {
+        var targets = context.ParseResult.CommandResult.Tokens.Select(token => token.Value);
+        var options = new Options(Bullseye.Options.Definitions.Select(definition => (definition.Aliases[0],
+            context.ParseResult.GetValueForOption(Options.OfType<Option<bool>>()
+                .Single(option => option.HasAlias(definition.Aliases[0]))))));
+        await RunTargetsWithoutExitingAsync(targets, options);
+    }
+}
+
+internal static class Targets
+{
+    public const string RunTestsCoverage = "run-tests-coverage";
+    public const string RestoreTools = "restore-tools";
+    public const string CleanBuildOutput = "clean-build-output";
+    public const string CleanArtifactsOutput = "clean-artifacts-output";
+    public const string CleanTestsOutput = "clean-test-output";
+    public const string CleanAll = "clean";
+    public const string Build = "build";
+    public const string RunTests = "run-tests";
+    public const string Pack = "pack";
+    public const string PublishArtifacts = "publish-artifacts";
+}
--- Content/build/Program.cs
+++ Content/build/Program.cs
@@ -1,136 +1,97 @@
 ﻿namespace build;
 
 using System.CommandLine;
-using System.CommandLine.Invocation;
-using Bullseye;
-using Commands;
-using static Bullseye.Targets;
-using static SimpleExec.Command;
+using System.CommandLine.Builder;
+using System.CommandLine.Help;
+using System.CommandLine.Hosting;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
+using System.Diagnostics;
+using System.Reflection;
+using Extensions.Options.AutoBinder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 internal static class Program
 {
-    private const string ArtifactsDirectory = ".artifacts";
-    private const string TestResultsDirectory = ".test-results";
+    public static string DefaultConfigFile = "buildconfig.json";
 
-    private static readonly Option<string> AdditionalArgsOption = new(new[] { "--additional-args", "-a" },
-        "Additional arguments to supply for target execution");
-
-    private static readonly Option<string> ConfigurationOption =
-        new(new[] { "--configuration", "-C" }, () => "Release", "The configuration to run the target");
+    public static Option<FileInfo> ConfigurationFileGlobalOption =
+        new("--config-file", "Specify configuration file");
 
     private static async Task<int> Main(string[] args)
     {
-        var command = new RootCommand { AdditionalArgsOption, ConfigurationOption };
-
-        command.ImportBullseyeConfigurations();
-
-        command.SetHandler(async context =>
+        try
         {
-            Target(Targets.RestoreTools, async () => { await RunAsync("dotnet", "tool restore"); });
+            var command = new RootCommand();
+            RegisterCommands(command);
+            command.AddGlobalOption(ConfigurationFileGlobalOption);
 
-            Target(Targets.CleanArtifactsOutput, () =>
+            command.SetHandler(context =>
             {
-                if (Directory.Exists(ArtifactsDirectory))
+                // ref: https://github.com/dotnet/command-line-api/issues/1537
+                context.HelpBuilder.CustomizeLayout(_ => HelpBuilder.Default.GetLayout().Skip(1));
+                context.HelpBuilder.Write(context.ParseResult.CommandResult.Command,
+                    context.Console.Out.CreateTextWriter());
+            });
+
+            var parser = new CommandLineBuilder(command)
+                .UseHost(CreateHostBuilder)
+                .UseDefaults()
+                .UseExceptionHandler((exception, context) =>
                 {
-                    Directory.Delete(ArtifactsDirectory, true);
-                }
-            });
+                    Console.WriteLine($"Unhandled exception occurred: {exception.Message}");
+                    context.ExitCode = 1;
+                })
+                .Build();
 
-            Target(Targets.CleanTestsOutput, () =>
-            {
-                if (Directory.Exists(TestResultsDirectory))
-                {
-                    Directory.Delete(TestResultsDirectory, true);
-                }
-            });
-
-            Target(Targets.CleanBuildOutput, async () =>
-            {
-                var configuration = context.ParseResult.GetValueForOption(ConfigurationOption);
-                await RunAsync("dotnet", $"clean -c {configuration} -v m --nologo");
-            });
-
-            Target(Targets.CleanAll,
-                DependsOn(Targets.CleanArtifactsOutput, Targets.CleanTestsOutput, Targets.CleanBuildOutput));
-
-            Target(Targets.Build, DependsOn(Targets.CleanBuildOutput), async () =>
-            {
-                var configuration = context.ParseResult.GetValueForOption(ConfigurationOption);
-                await RunAsync("dotnet", $"build -c {configuration} --nologo");
-            });
-
-            Target(Targets.Pack, DependsOn(Targets.CleanArtifactsOutput, Targets.Build), async () =>
-            {
-                var configuration = context.ParseResult.GetValueForOption(ConfigurationOption);
-                await RunAsync("dotnet",
-                    $"pack -c {configuration} -o {Directory.CreateDirectory(ArtifactsDirectory).FullName} --no-build --nologo");
-            });
-
-            Target(Targets.PublishArtifacts, DependsOn(Targets.Pack), () => Console.WriteLine("publish artifacts"));
-
-            Target("default", DependsOn(Targets.RunTests, Targets.PublishArtifacts));
-
-            Target(Targets.RunTests, DependsOn(Targets.CleanTestsOutput, Targets.Build), async () =>
-            {
-                var configuration = context.ParseResult.GetValueForOption(ConfigurationOption);
-                await RunAsync("dotnet",
-                    $"test -c {configuration} --no-build --nologo --collect:\"XPlat Code Coverage\" --results-directory {TestResultsDirectory}");
-            });
-
-            Target(Targets.RunTestsCoverage, DependsOn(Targets.RestoreTools, Targets.RunTests), () =>
-                Run("dotnet",
-                    $"reportgenerator -reports:{TestResultsDirectory}/**/*cobertura.xml -targetdir:{TestResultsDirectory}/coveragereport -reporttypes:HtmlSummary"));
-
-            Target(Targets.Ping, async () => { await context.InvokeCommandAsync<PingCommand>(); });
-
-
-            await command.RunBullseyeAndExitAsync(context);
-        });
-
-        return await command.InvokeAsync(args);
-    }
-
-    private static void ImportBullseyeConfigurations(this Command command)
-    {
-        // translate from Bullseye to System.CommandLine
-        command.Add(new Argument<string[]>("targets")
+            return await parser.InvokeAsync(args);
+        }
+        catch (Exception exception)
         {
-            Description =
-                "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed. Target names may be abbreviated. For example, \"b\" for \"build\"."
-        });
-
-        foreach (var (aliases, description) in Options.Definitions)
-            command.Add(new Option<bool>(aliases.ToArray(), description));
+            Console.WriteLine($"Application terminated unexpectedly: {exception.Message}");
+            return 1;
+        }
     }
 
-    private static async Task<int> InvokeCommandAsync<TCommand>(this InvocationContext context)
-        where TCommand : Command, new()
+    public static IHostBuilder CreateHostBuilder(string[] args)
     {
-        var additionalArguments = context.ParseResult.GetValueForOption(AdditionalArgsOption);
-        return await new TCommand().InvokeAsync(additionalArguments!, context.Console);
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var fileInfo = hostingContext.GetInvocationContext().ParseResult
+                    .GetValueForOption(ConfigurationFileGlobalOption);
+
+                // https://www.hanselman.com/blog/how-do-i-find-which-directory-my-net-core-console-application-was-started-in-or-is-running-from
+                // https://stackoverflow.com/a/97491/7644876
+                var basePath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule!.FileName);
+
+                var configFilePath = fileInfo is { Exists: true }
+                    ? fileInfo.FullName
+                    : DefaultConfigFile;
+
+                config
+                    .SetBasePath(basePath!)
+                    .AddJsonFile(configFilePath, true, false)
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureLogging((context, builder) =>
+            {
+                builder.ClearProviders();
+                builder.AddConsole().SetMinimumLevel(LogLevel.Trace);
+            })
+            .ConfigureServices((hostContext, services) => { services.AutoBindOptions(); });
     }
 
-    private static async Task RunBullseyeAndExitAsync(this Command command, InvocationContext context)
+    private static void RegisterCommands(Command rootCommand)
     {
-        var targets = context.ParseResult.CommandResult.Tokens.Select(token => token.Value);
-        var options = new Options(Options.Definitions.Select(d => (d.Aliases[0],
-            context.ParseResult.GetValueForOption(command.Options.OfType<Option<bool>>()
-                .Single(o => o.HasAlias(d.Aliases[0]))))));
-        await RunTargetsAndExitAsync(targets, options);
-    }
-}
+        var types = Assembly.GetExecutingAssembly().GetTypes().Where(type =>
+            type.IsClass &&
+            !type.IsAbstract &&
+            type.IsSubclassOf(typeof(Command)) &&
+            type.GetConstructors().Any(info => !info.GetParameters().Any()));
 
-internal static class Targets
-{
-    public const string RunTestsCoverage = "run-tests-coverage";
-    public const string RestoreTools = "restore-tools";
-    public const string CleanBuildOutput = "clean-build-output";
-    public const string CleanArtifactsOutput = "clean-artifacts-output";
-    public const string CleanTestsOutput = "clean-test-output";
-    public const string CleanAll = "clean";
-    public const string Build = "build";
-    public const string RunTests = "run-tests";
-    public const string Pack = "pack";
-    public const string PublishArtifacts = "publish-artifacts";
-    public const string Ping = "ping";
+        foreach (var type in types) rootCommand.AddCommand((Command)Activator.CreateInstance(type)!);
+    }
 }
